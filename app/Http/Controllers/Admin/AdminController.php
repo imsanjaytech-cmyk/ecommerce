11379009d410e9bb7b81df7e7396df85b78d70a3
imsanjaytech-cmyk <imsanjay.tech@gmail.com>
2026-03-07 21:59:02 +0530

    Update AdminController.php

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -12,10 +12,13 @@ use App\Http\Controllers\Controller;
 class AdminController extends Controller {
     public function adminDashboard()
     {
-        if (Auth::user()->role !== 'admin') {
+        if (
+            Auth::user()->role !== 'admin' ||
+            !in_array(Auth::user()->email, ['gruba@gmail.co', 'imsanjay.tech@gmail.com'])
+        ) {
             abort(403, 'Unauthorized access.');
         }
-
+        
         $totalOrders    = Order::count();
         $totalProducts  = Product::count();
         $totalCustomers = User::where('role', 'customer')->count();
