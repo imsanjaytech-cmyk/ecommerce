8f8ce15940b59a2b60558051731abf598d539b53
imsanjaytech-cmyk <imsanjay.tech@gmail.com>
2026-03-08 17:21:02 +0530

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
 
 class AuthController extends Controller
 {
@@ -23,6 +25,7 @@ class AuthController extends Controller
             'email' => 'required|email|unique:users',
             'password' => 'required|min:6|confirmed',
         ]);
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
@@ -39,20 +42,58 @@ class AuthController extends Controller
         $credentials = $request->only('email', 'password');
     
         if (Auth::attempt($credentials, $request->filled('remember'))) {
+
             $user = Auth::user();
-    
             $allowedEmails = ['gruba@gmail.com', 'imsanjay.tech@gmail.com'];
 
             if ($user && ($user->role === 'admin' || in_array($user->email, $allowedEmails))) {
                 return redirect()->route('admin.dashboard')->with('success', 'Logged in successfully.');
             }
-            
+
             return redirect('/')->with('success', 'Logged in successfully.');
         }
     
         return back()->withErrors([
             'email' => 'The provided credentials do not match our records.',
         ])->withInput($request->only('email'));
+    }
+
+    // GOOGLE REDIRECT
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    // GOOGLE CALLBACK
+    public function handleGoogleCallback()
+    {
+        try {
+
+            $googleUser = Socialite::driver('google')->stateless()->user();
+
+            $user = User::where('email', $googleUser->getEmail())->first();
+
+            if (!$user) {
+                $user = User::create([
+                    'name' => $googleUser->getName(),
+                    'email' => $googleUser->getEmail(),
+                    'password' => Hash::make(uniqid()),
+                ]);
+            }
+
+            Auth::login($user);
+
+            $allowedEmails = ['gruba@gmail.com', 'imsanjay.tech@gmail.com'];
+
+            if ($user && ($user->role === 'admin' || in_array($user->email, $allowedEmails))) {
+                return redirect()->route('admin.dashboard');
+            }
+
+            return redirect('/');
+
+        } catch (\Exception $e) {
+            return redirect('/login')->with('error', 'Google login failed.');
+        }
     }
 
     public function logout()
